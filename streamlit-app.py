1881d8d37152873e0b7d8677bbca0d454fce2652
H-MIMBANG <heddy.mimbang@icloud.com>
2025-01-23 11:03:44 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -19,3 +19,14 @@ st.subheader('Histogramme de la longueur des sépales')
 plt.figure(figsize=(10,5))
 sns.histplot(df['SepalLength'], bins=20, kde=True)
 st.pyplot(plt)
+
+# About
+
+if st.button("About App"):
+	st.subheader("App d'exploration des données d'Iris")
+	st.text("Construite avec Streamlit")
+	st.text("Thanks to the Streamlit Team Amazing Work")
+
+if st.checkbox("By"):
+	st.text("Heddy MIMBANG")
+	st.text("heddy.mimbang@icloud.com")
